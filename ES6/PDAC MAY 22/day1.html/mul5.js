3b3372b752b508fc5cdb55b91dc3354d1bb47243
fardeenkhan123 <106161230+fardeenkhan123@users.noreply.github.com>
2022-08-27 00:09:02 +0530

    gani

---
+++ ES6/PDAC MAY 22/day1.html/mul5.js
@@ -0,0 +1,6 @@
+function getmul5(
+    n1,n2,n3,n5,
+){
+    return n1 * n2 * n3 * n4 * n5*
+    
+}
---
+++ ES6/PDAC MAY 22/day1.html/sqrnum.js
@@ -0,0 +1,7 @@
+function getsquare(base){
+    return base**2;
+}
+
+const result =
+getsure(11);
+console,log(result);
