e24a6d9b73a3bf1c0d60131296fea76c7ba79595
fardeenkhan123 <106161230+fardeenkhan123@users.noreply.github.com>
2022-11-18 19:36:59 +0530

    rrr

---
+++ objapi/apiapp/src/Component/Array.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Array=()=>{
+
+    var a=[1,2,3,4,,4]
+    console.log(a)                  
+    return(
+        <div>
+            <h1>HII</h1>
+
+            
+       </div>
+    )
+}
